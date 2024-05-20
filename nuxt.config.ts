a3ea8aa40de710d0d2fead47800d67571fd12f78
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-20 20:50:58 +0200

    fixed mobile developement for IOS finally!!!!!!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: false,
+  },
+  ssr: false,
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  vite: {
+    clearScreen: false,
+    server: {
+      strictPort: true,
+      hmr: {
+        host: '0.0.0.0',
+        protocol: 'ws',
+        port: 3001,
+      },
+    },
+  },
 })
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
