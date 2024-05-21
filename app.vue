7d17e71d8f78ad7ac26e9200a360a4aee726925a
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-21 19:47:55 +0200

    disabled zooming and pinching

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <Navbar/>
-  <Player/>
+  <Navbar />
+  <Player />
   <NuxtPage />
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+  app:{
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        }
+      ]
+    },
+  },
   devtools: {
     enabled: false,
   },
