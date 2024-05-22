857a459921d420d76301fc970c0dbc71b38673e2
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-05-22 17:26:49 +0200

    added types

---
+++ types/types.ts
@@ -0,0 +1,100 @@
+export interface Song {
+  id: string;
+  title: string;
+  artist: string;
+  length: number;
+  cover: string;
+  coverURL?: string;
+  date_added: string;
+  lastPlayed?: string;
+}
+
+export interface Playlist {
+  id: string;
+  name: string;
+  date: string;
+  cover: string;
+  songs: string[];
+}
+
+export interface SongsConfig {
+  songs: Record<string, Song>;
+  playlists: Record<string, Playlist>;
+}
+
+export interface Player {
+  audio: HTMLAudioElement;
+  currentSongId: string;
+  audioContext: AudioContext | null;
+  sourceNode: MediaElementAudioSourceNode | null;
+  eqFilters: BiquadFilterNode[];
+}
+
+export interface MusicSearchResponseItem {
+  url: string;
+  type: string;
+  title: string;
+  thumbnail: string;
+  uploaderName: string;
+  uploaderUrl: string;
+  uploaderAvatar: any;
+  uploadedDate: any;
+  shortDescription: any;
+  duration: number;
+  views: number;
+  uploaded: number;
+  uploaderVerified: boolean;
+  isShort: boolean;
+}
+
+export interface MusicSearchResponse {
+  items: MusicSearchResponseItem[];
+}
+
+export interface EQSettings {
+  "32": string;
+  "64": string;
+  "125": string;
+  "250": string;
+  "500": string;
+  "1000": string;
+  "2000": string;
+  "4000": string;
+  "8000": string;
+  "16000": string;
+}
+
+export type EQSettingsKeys = keyof EQSettings;
+
+export type EQ = keyof EQSettings;
+
+export interface PlayerSettings {
+  volume: number;
+  currentSong: string;
+  eq: EQSettings;
+}
+
+export interface UserSettings {
+  playerSettings: PlayerSettings;
+  apiURL: string;
+}
+
+export const defaultSettings: UserSettings = {
+  playerSettings: {
+    volume: 50,
+    currentSong: "",
+    eq: {
+      "32": "0.0",
+      "64": "0.0",
+      "125": "0.0",
+      "250": "0.0",
+      "500": "0.0",
+      "1000": "0.0",
+      "2000": "0.0",
+      "4000": "0.0",
+      "8000": "0.0",
+      "16000": "0.0",
+    },
+  },
+  apiURL: "",
+};
